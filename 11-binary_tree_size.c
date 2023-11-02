a6a66b81e06f34aaaca7e7c06e2a661face96633
McMmie <tharausamuel01@gmail.com>
2023-11-02 18:07:00 +0000

    main file

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,4 +1,4 @@
-#include "binary_trees.h>"
+#include "binary_trees.h"
 
 /**
  * binary_tree_size - measures the size of a binary tree
@@ -13,3 +13,4 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		return (0);
 
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
