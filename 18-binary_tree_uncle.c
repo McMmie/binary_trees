03a1ef9fb65b3955a61657b1b45ca5b5576e7c29
McMmie <tharausamuel01@gmail.com>
2023-11-02 23:30:51 +0000

    added base case

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,17 +9,17 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t * daddy;
+	binary_tree_t *daddy;
 
-	 if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-		 return (NULL);
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 
-	 daddy = node->parent->parent;
+	daddy = node->parent->parent;
 
-	 if (daddy-> left == node->parent)
-		 return (daddy->right);
-	 else if (daddy->right == node->parent)
-		 return (daddy->left);
+	if (daddy->left == node->parent)
+		return (daddy->right);
+	else if (daddy->right == node->parent)
+		return (daddy->left);
 
 	return (NULL);
 }
