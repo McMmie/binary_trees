c9cfadf0ebfa3593723938c21d0e66ece6a712a6
McMmie <tharausamuel01@gmail.com>
2023-11-02 17:49:01 +0000

    correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left, right;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
